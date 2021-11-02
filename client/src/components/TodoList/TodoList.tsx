eb123b005edec03349930ff6ca404b9dbc434adb
RichardHpa <richard.hpa@hotmail.com>
2021-11-02 21:42:53 +1300

    user can delete a todo

--- client/src/components/TodoList/TodoList.tsx
+++ client/src/components/TodoList/TodoList.tsx
@@ -1,10 +1,27 @@
-import { useQuery } from 'react-query';
-import { getAllTodos } from 'api/todos';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { getAllTodos, deleteTodo } from 'api/todos';
 import { IconButton, List, ListItem, ListItemText } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useSnackbar } from 'notistack';
 
 const TodoList = () => {
   const { data } = useQuery('todos', getAllTodos);
+  const queryClient = useQueryClient();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const { mutateAsync } = useMutation(deleteTodo, {
+    onSuccess: (res) => {
+      queryClient.invalidateQueries('todos');
+      enqueueSnackbar('Successfully removed Todo', { variant: 'success' });
+    },
+    onError: () => {
+      enqueueSnackbar('Something went wrong.', { variant: 'error' });
+    }
+  });
+
+  const handleDelete = async (id: number) => {
+    await mutateAsync({ id });
+  };
 
   return (
     <List>
@@ -12,7 +29,11 @@ const TodoList = () => {
         <ListItem
           key={todo.todo_id}
           secondaryAction={
-            <IconButton edge="end" aria-label="delete">
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={() => handleDelete(todo.todo_id)}
+            >
               <DeleteIcon />
             </IconButton>
           }
